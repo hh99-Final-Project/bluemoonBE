1e5c136a9a8080b9b649970b6c58dbb5ed869323
jaeyoungjang2 <lion85488@gmail.com>
2022-05-10 22:23:26 +0900

    [#83] feat: application properties 수정

--- src/main/java/com/sparta/util/JasyptConfig.java
+++ src/main/java/com/sparta/util/JasyptConfig.java
@@ -1,6 +1,5 @@
 package com.sparta.util;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -8,15 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableEncryptableProperties
 public class JasyptConfig {
 
-	@Bean(name = "jasyptStringEncryptor")
+	@Bean("jasyptStringEncryptor")
 	public StringEncryptor stringEncryptor() {
-		String password = System.getenv("JASYPT_ENCRYPTOR_PASSWORD");
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(password); // 암호화할 때 사용하는 키
+		config.setPassword(System.getenv("JASYPT_ENCRYPTOR_PASSWORD")); // 암호화할 때 사용하는 키
 		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
 		config.setKeyObtentionIterations(System.getenv("JASYPT_ENCRYPTOR_REPEAT")); // 반복할 해싱 회수
 		config.setPoolSize("1"); // 인스턴스 pool
@@ -25,7 +22,7 @@ public class JasyptConfig {
 		config.setStringOutputType("base64"); //인코딩 방식
 		encryptor.setConfig(config);
 
-
 		return encryptor;
 	}
 }
+
