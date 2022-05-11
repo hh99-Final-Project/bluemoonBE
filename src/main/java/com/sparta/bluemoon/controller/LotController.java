f38885602aff245eb5d7210c257a5c8a2d8a20b3
Bongjin Choi <cbjjzzang@naver.com>
2022-05-11 19:31:34 +0900

    [#121]feat:채팅방 입장시 상대유저 정보 확인 채팅방 입장시 상대유저 정보 확인 코드 수정합니다.

--- src/main/java/com/sparta/bluemoon/controller/LotController.java
+++ src/main/java/com/sparta/bluemoon/controller/LotController.java
@@ -1,27 +1,17 @@
 package com.sparta.bluemoon.controller;
 
-import com.sparta.bluemoon.domain.Point;
-import com.sparta.bluemoon.domain.User;
 import com.sparta.bluemoon.dto.response.LotResponseDto;
-import com.sparta.bluemoon.repository.PointRepository;
 import com.sparta.bluemoon.security.UserDetailsImpl;
 import com.sparta.bluemoon.service.LotService;
-import com.sparta.bluemoon.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.transaction.Transactional;
-import java.util.Random;
 
 @RequiredArgsConstructor
 @RestController
 public class LotController {
     private final LotService lotService;
-
-
 
     @GetMapping("/api/lot")
     public LotResponseDto lotResult(@AuthenticationPrincipal UserDetailsImpl userDetails) {
