f27031030d5112ecd965022baf7d068417add6e5
TodayIsYolo <a1991015@gmail.com>
2022-05-12 22:39:09 +0900

    [#149] feat : 추천인 등록 추천인 1000p, 회원가입자 500p

--- src/main/java/com/sparta/bluemoon/repository/UserRepository.java
+++ src/main/java/com/sparta/bluemoon/repository/UserRepository.java
@@ -10,5 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickname(String nickname);
 
     Optional<User> findByUsername(String email);
+    Optional<User> findByNickname(String nickname);
 
 }
