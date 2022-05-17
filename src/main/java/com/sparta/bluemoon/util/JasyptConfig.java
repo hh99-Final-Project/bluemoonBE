6fd226b6632ea46c28eb4d1a1b2140c4765c40a6
jaeyoungjang2 <lion85488@gmail.com>
2022-05-17 22:10:27 +0900

    redis 주석 제거

--- src/main/java/com/sparta/bluemoon/util/JasyptConfig.java
+++ src/main/java/com/sparta/bluemoon/util/JasyptConfig.java
@@ -21,9 +21,9 @@ public class JasyptConfig {
 //        String password = System.getenv("JASYPT_ENCRYPTOR_PASSWORD");
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(System.getenv("JASYPT_ENCRYPTOR_PASSWORD")); // 암호화할 때 사용하는 키
+		config.setPassword("7a4e71ac-db50-4635-a4e6-9d906f97654e"); // 암호화할 때 사용하는 키
 		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
-		config.setKeyObtentionIterations(System.getenv("JASYPT_ENCRYPTOR_REPEAT")); // 반복할 해싱 회수
+		config.setKeyObtentionIterations(691); // 반복할 해싱 회수
 		config.setPoolSize("1"); // 인스턴스 pool
 		config.setProviderName("SunJCE");
 		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
