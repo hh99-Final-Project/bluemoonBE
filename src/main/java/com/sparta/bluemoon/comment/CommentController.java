0939d38b0c45a00d01aff619261fbe241cd773b1
Bongjin Choi <cbjjzzang@naver.com>
2022-05-25 13:42:32 +0900

    refactor:commentSaveError 댓글 저장시 글과 보이스가 둘다 입력되지 않았을 경우 에러 발생

--- src/main/java/com/sparta/bluemoon/comment/CommentController.java
+++ src/main/java/com/sparta/bluemoon/comment/CommentController.java
@@ -2,15 +2,16 @@ package com.sparta.bluemoon.comment;
 
 import com.sparta.bluemoon.comment.requestDto.CommentRequestDto;
 import com.sparta.bluemoon.comment.responseDto.CommentResponseDto;
+import com.sparta.bluemoon.exception.CustomException;
 import com.sparta.bluemoon.security.UserDetailsImpl;
 import com.sparta.bluemoon.util.VoiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
 import java.io.IOException;
+
+import static com.sparta.bluemoon.exception.ErrorCode.PLEASE_ENTER_TEXT_OR_VOICE;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,9 +24,12 @@ public class CommentController {
     // 댓글 저장할떄는 상위 댓글의 정보를 받아와야한다.
     // 최상위 댓글의 경우 상위 댓글이 존재하지 않으므로 값을 빈칸으로 보내주면 된다.
     @PostMapping(value = "/api/comments", consumes = {"multipart/form-data"})
-    public CommentResponseDto saveComment(@Valid @RequestPart(required = false) CommentRequestDto requestDto,
+    public CommentResponseDto saveComment(@RequestPart(required = false) CommentRequestDto requestDto,
                                           @RequestPart(required = false) MultipartFile file,
                                           @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        //댓글이 빈칸으로 들어왔을 경우 에러 발생
+        if(requestDto.getContent() == null && file == null) throw new CustomException(PLEASE_ENTER_TEXT_OR_VOICE);
+
         String voiceUrl = "";
         if(file != null){
             voiceUrl = voiceService.upload(file,"static");
